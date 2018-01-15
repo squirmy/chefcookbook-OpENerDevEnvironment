a0b11f27982627f5e7772f2f43b5c13e24150bc7
Adam Woods <adam.tomato@gmail.com>
2018-01-15 11:47:22 +1100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,6 @@ long_description 'Installs/Configures OpENer'
 version '0.1.0'
 
 depends 'git', '~> 8.0.0'
-depends 'build-essential', '~> 2.2.3'
+depends 'build-essential', '~> 8.0.4'
 depends 'cmake', '~> 0.3.0'
 depends 'CppUTest', '~> 0.1.0'
