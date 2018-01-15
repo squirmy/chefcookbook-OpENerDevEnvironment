2fb14079a6caa740dccba25c62998c1983982992
Adam Woods <adam.tomato@gmail.com>
2018-01-15 12:25:03 +1100

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,5 @@
 default['OpENer']['url'] = 'https://github.com/EIPStackGroup/OpENer'
 default['OpENer']['folder'] = '/home/vagrant/OpENer'
+
+override["cmake"]["source"]["version"] = "3.10.1"
+override["cmake"]["source"]["checksum"] = "7be36ee24b0f5928251b644d29f5ff268330a916944ef4a75e23ba01e7573284"
